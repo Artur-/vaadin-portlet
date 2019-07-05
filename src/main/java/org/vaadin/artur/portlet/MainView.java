db0e4e99ad817f243f1f74fa1414ffd99650d098
Artur Signell <artur@vaadin.com>
2019-07-05 15:56:54 +0300

    Show portlet and portal info

--- src/main/java/org/vaadin/artur/portlet/MainView.java
+++ src/main/java/org/vaadin/artur/portlet/MainView.java
@@ -3,12 +3,16 @@ package org.vaadin.artur.portlet;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.portal.VaadinPortlet;
 
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        Button button = new Button("Click me",
-                event -> Notification.show("Clicked!"));
+        VaadinPortlet portlet = VaadinPortlet.getCurrent();
+        String name = portlet.getPortletName();
+        String serverInfo = portlet.getPortletContext().getServerInfo();
+        Button button = new Button("Click me", event -> Notification.show(
+                "Hello from " + name + " running in " + serverInfo + "!"));
         add(button);
     }
 }
