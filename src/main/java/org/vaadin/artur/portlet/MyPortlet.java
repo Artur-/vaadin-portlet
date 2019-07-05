fe7d9c6df8c6a5ec6c272a32653812d70770b492
Artur Signell <artur@vaadin.com>
2019-07-05 15:52:48 +0300

    Workaround to find static resources

--- src/main/java/org/vaadin/artur/portlet/MyPortlet.java
+++ src/main/java/org/vaadin/artur/portlet/MyPortlet.java
@@ -3,6 +3,10 @@ package org.vaadin.artur.portlet;
 import com.vaadin.flow.portal.VaadinPortlet;
 
 public class MyPortlet extends VaadinPortlet {
-
+    @Override
+    public String getName() {
+        // This corresponds to the war filename
+        return "vaadin-test-portlet";
+    }
 
 }
